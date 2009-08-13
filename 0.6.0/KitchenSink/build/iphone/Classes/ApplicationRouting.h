bb5b0d38e380882e38e168f2bb90953b1989807a
Kevin Whinnery <kevin@EVE.local>
2009-08-12 21:14:28 -0500

    New Kitchen Sink for 0.6.0

---
+++ 0.6.0/KitchenSink/build/iphone/Classes/ApplicationRouting.h
@@ -0,0 +1,72 @@
+/**
+ * Appcelerator Titanium Mobile
+ * This is generated code. Do not modify. Your changes will be lost.
+ * Generated code is Copyright (c) 2009 by Appcelerator, Inc.
+ * All Rights Reserved.
+ */
+#import <Foundation/Foundation.h>
+
+@protocol TitaniumAppAssetResolver
+- (NSData*) resolveAppAsset:(NSURL*)url;
+- (oneway void)release;
+- (id)retain;
+@end
+
+@interface ApplicationRouting : NSObject<TitaniumAppAssetResolver> {
+}
+- (NSData*) resolveAppAsset:(NSURL*)url;
+- (NSData*) pageNamedAccelerometer;
+- (NSData*) pageNamedActivity_indicators;
+- (NSData*) pageNamedAnimated_false;
+- (NSData*) pageNamedApplication_data;
+- (NSData*) pageNamedButtonbar;
+- (NSData*) pageNamedButtons;
+- (NSData*) pageNamedComposite;
+- (NSData*) pageNamedDatabase;
+- (NSData*) pageNamedDynamic_content;
+- (NSData*) pageNamedEmail;
+- (NSData*) pageNamedFilesystem;
+- (NSData*) pageNamedFullscreen;
+- (NSData*) pageNamedGeo;
+- (NSData*) pageNamedImage;
+- (NSData*) scriptNamedIndex;
+- (NSData*) pageNamedIphone_ui;
+- (NSData*) pageNamedNavbar_color;
+- (NSData*) pageNamedNavbar_hide;
+- (NSData*) pageNamedNavbar_left;
+- (NSData*) pageNamedNavbar_right;
+- (NSData*) pageNamedNavbar_title;
+- (NSData*) pageNamedOrientation;
+- (NSData*) pageNamedPhone;
+- (NSData*) pageNamedPlatform;
+- (NSData*) pageNamedPlatform_data;
+- (NSData*) pageNamedProgress_bars;
+- (NSData*) pageNamedProperties;
+- (NSData*) pageNamedShake;
+- (NSData*) pageNamedSlider;
+- (NSData*) pageNamedSound;
+- (NSData*) pageNamedStatusbar;
+- (NSData*) pageNamedSwitch;
+- (NSData*) pageNamedTabbar;
+- (NSData*) pageNamedTabbar_hide;
+- (NSData*) pageNamedTableview_add;
+- (NSData*) pageNamedTableview_delete;
+- (NSData*) pageNamedTableview_set;
+- (NSData*) pageNamedTableview_update;
+- (NSData*) pageNamedTextfield_events;
+- (NSData*) pageNamedTextfields;
+- (NSData*) pageNamedToolbar_fixedspace;
+- (NSData*) pageNamedToolbar_hideshow;
+- (NSData*) pageNamedToolbar_left;
+- (NSData*) pageNamedToolbar_lots;
+- (NSData*) pageNamedToolbar_middle;
+- (NSData*) pageNamedToolbar_right;
+- (NSData*) pageNamedUi;
+- (NSData*) pageNamedViews;
+- (NSData*) pageNamedViews1;
+- (NSData*) pageNamedViews2;
+- (NSData*) pageNamedViews3;
+- (NSData*) pageNamedWindow_unfocus;
+- (NSData*) pageNamedXhr;
+
+@end
